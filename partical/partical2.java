c92c134898c593b215303f9e815503ee5c63fb10
shiughodke <105426397+shiughodke@users.noreply.github.com>
2022-05-17 17:38:25 +0530

    Add files via upload

---
+++ partical/partical2.java
@@ -0,0 +1,17 @@
+package partical2;
+
+public class partical2 {
+
+	public static void main(String[] args) {
+		byte myByte1,myByte2;
+		myByte1 = 127;
+		myByte2 = -48;
+		System.out.println("Byte1 : " +myByte1);
+		
+		System.out.println("Byte2 : " +myByte2);
+		
+			
+
+	}
+
+}
