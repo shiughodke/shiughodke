a35281a768552a09840c89f0aa7b098d58008da1
shiughodke <105426397+shiughodke@users.noreply.github.com>
2022-05-18 16:17:07 +0530

    Add files via upload

---
+++ partical/logical.java
@@ -0,0 +1,19 @@
+package logical;
+
+public class logical {
+
+	public static void main(String[] args) {
+		//Variables Definition and Initialization
+		  boolean a = true, b = false;
+		  //Logical AND
+		  System.out.println("a1 && b = " + (a && b));
+		  
+		  //Logical OR
+		  System.out.println("a || b = " + (a || b) );
+		  
+		  //Logical Not
+		  System.out.println("!(a && b) = " + !(a && b));
+
+	}
+
+}
