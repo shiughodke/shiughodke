72e76c43b8bd030270de1e03ae2c2bb769227697
shiughodke <105426397+shiughodke@users.noreply.github.com>
2022-05-20 17:16:45 +0530

    Add files via upload

---
+++ partical/Demo.java
@@ -0,0 +1,15 @@
+
+public class Demo {
+
+	public Demo() {
+		System.out.println("constructor is no argument type");
+	}
+
+	public static void main(String[] args) {
+		new Demo();
+		
+
+
+	}
+
+}
---
+++ partical/MyDefaultConstructor.java
@@ -0,0 +1,16 @@
+
+public class MyDefaultConstructor {
+
+	public MyDefaultConstructor() {
+ System.out.println("this is default constructor");
+
+		
+	}
+
+	public static void main(String[] args) {
+		MyDefaultConstructor mdc = new MyDefaultConstructor();
+		
+
+	}
+
+}
