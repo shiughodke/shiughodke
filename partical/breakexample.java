82298e0e9205380eec71c0e3e639f011f012da43
shiughodke <105426397+shiughodke@users.noreply.github.com>
2022-05-23 10:02:54 +0530

    Add files via upload

---
+++ partical/breakexample.java
@@ -0,0 +1,16 @@
+package breakexample;
+
+public class breakexample {
+
+	public static void main(String[] args) {
+		for(int i=2;i<=8;i++) {
+			if(i==6) {
+				break;
+				
+			}
+			System.out.println(i);
+		}
+
+	}
+
+}
