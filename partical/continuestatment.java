6b69da628c55d5e3e5cf04d192bbf48faf775173
shiughodke <105426397+shiughodke@users.noreply.github.com>
2022-05-24 14:52:54 +0530

    Add files via upload

---
+++ partical/continuestatment.java
@@ -0,0 +1,24 @@
+
+public class continuestatment {
+
+	public static void main(String args[]) {
+		for(int j=1;j<=5;j++)
+		{
+			if(j==3)
+			{
+				continue;
+				
+			}
+				System.out.println(j+"");
+				
+				
+				
+			
+			
+				
+		}
+			
+
+	}
+
+}
