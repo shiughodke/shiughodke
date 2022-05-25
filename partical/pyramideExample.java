966447e22aeea9f75598c4f1811d799340e23f85
shiughodke <105426397+shiughodke@users.noreply.github.com>
2022-05-25 17:56:41 +0530

    Add files via upload

---
+++ partical/pyramideExample.java
@@ -0,0 +1,27 @@
+package hello;
+
+public class pyramideExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+for(int i=1;i<=5;i++) {//outer loop
+	for(int j=1;j<=i;j++){//inner loop
+		System.out.print(" * ");
+		
+		
+	}
+	System.out.println();//new line
+}
+//
+ for(int i=1;i<=5;i++) {
+	 for(int j=5;j>=i;j--) {
+		 System.out.print(" * ");
+	 }
+	 System.out.println();
+ }
+		
+		
+	}
+
+}
+                               
