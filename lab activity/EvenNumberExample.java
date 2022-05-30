e19941c1faecc175ab2e0330188c4b6bd21ac744
shiughodke <105426397+shiughodke@users.noreply.github.com>
2022-05-30 22:03:55 +0530

    Add files via upload

---
+++ lab activity/EvenNumberExample.java
@@ -0,0 +1,24 @@
+package hello;
+import java.util.*;
+
+public class EvenNumberExample {
+
+	public static void main(String[] args) {
+		int n, sun=0;
+		System.out.println(" Enter a number:");
+		Scanner s = new Scanner(System.in);
+		n=s.nextInt();
+		for(int i=1;i<=n;i++) {
+			if(i % 2 == 0) {
+				
+			}
+		}
+		
+		
+		
+System.out.println("sum of even numbers are :+sum");
+		
+
+	}
+
+}
