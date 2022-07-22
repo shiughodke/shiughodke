8655e4609f69372e49633a64b68143c1fb07d53e
shiughodke <105426397+shiughodke@users.noreply.github.com>
2022-07-22 14:28:30 +0530

    Add files via upload

--- lab activity/Employee.java
+++ lab activity/Employee.java
@@ -1,45 +1,38 @@
-package shiu;
+package New;
+
+import java.util.*;
 
 public class Employee {
-// instance variable
-	long employeeid ;
-	String employeeName;
-	String employeeAdders;
-	long employeePhone ;
-	double basicSalary;
-	double specialAllowance = 250.80; // default value
-	double hra = 1000.50; // default value
-
-
 	
+		public static void main(String[] args) {
+			
+			Scanner s = new Scanner(System.in); // creating Scanner object
+			 System.out.println("Enter salary");
+			int bsalary = s.nextInt(); // reading
+		    
 
-	// parameter constructor
+	       float hra,da,totalsalary,gross;
 
-	
-	public Employee(long Id, String Name, String Adders, long Phone) {
-		super();
-		this.employeeid = Id;
-		this.employeeName = Name;
-		this.employeeAdders = Adders;
-		this.employeePhone = Phone;
-	}
-
-// calculate basic Salary
-	
-	public void calculateSalary() {
-		double Salary; // local variable
-		Salary = basicSalary + (basicSalary * specialAllowance/100)+(basicSalary * hra/100);
-		
-		System.out.println("Employee Salary is : " + Salary);
-	}
-
-
-
-
-
-	
-	
+		if(bsalary>500) {
+			hra = 500;
+			da = 90;
+			totalsalary = hra * da * bsalary;
+			System.out.println("total salary :"+totalsalary);
+		}
+		else
+		{
+			hra = 200;
+		    da = 70;
+		    totalsalary = hra * da * bsalary;
+			System.out.println("total salary :"+totalsalary);
+	    }
+	    gross = bsalary+hra+da;
+		System.out.println("gross salary:"+gross);		
 
 	}
+    }
 
+
+	
+	
 
