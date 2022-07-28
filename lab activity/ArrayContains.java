699720fe55a1807257193627dc0a7c1233699569
shiughodke <105426397+shiughodke@users.noreply.github.com>
2022-07-28 17:56:11 +0530

    Add files via upload

---
+++ lab activity/ArrayContains.java
@@ -0,0 +1,15 @@
+package Lab;
+
+public class ArrayContains {
+	public static void main(String[] args) {
+		int []arr = {1,2,3,4,5,6}; //creating Array
+		int key = 4; //specific value
+		for(int i =0; i<arr.length;i++) {
+			if(key==arr[i]); //condition for checking specific value
+			System.out.println("Array contains specific value");
+		}
+		
+
+	}
+
+}
---
+++ lab activity/ArrayElement.java
@@ -0,0 +1,13 @@
+package Lab;
+
+public class ArrayElement {
+
+	public static void main(String[] args) {
+		int []arr = {1,2,3,4,5,6}; // creating array
+		System.out.println("Array element present in odd position");
+		for(int i =0; i<arr.length;i+=2) {
+			System.out.println("arr[i]");
+	}
+
+}
+}
